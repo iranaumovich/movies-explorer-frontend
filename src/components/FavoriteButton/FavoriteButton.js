1f7201eff7304c99700effdc0225dfdcd8034891
Iryna Tsupryk <iranaumovich0@gmail.com>
2023-08-06 20:15:32 +0200

    fix: установить фокус инпутам

--- src/components/FavoriteButton/FavoriteButton.js
+++ src/components/FavoriteButton/FavoriteButton.js
@@ -1,10 +1,19 @@
 import { ReactComponent as Icon } from "../../images/fav-button.svg";
 import { ReactComponent as IconActive } from "../../images/fav-button_active.svg";
 import "./style.css";
+import React from "react";
 
-function FavoriteButton({ active, onClick }) {
+function FavoriteButton() {
+  const [active, setActive] = React.useState(false);
+
   return (
-    <button className="favorite-button" type="button" onClick={onClick}>
+    <button
+      className="favorite-button"
+      type="button"
+      onClick={() => {
+        setActive(true);
+      }}
+    >
       {active ? <IconActive /> : <Icon />}
     </button>
   );
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -12,33 +12,35 @@ function Profile({ isEditing, onEditClick }) {
           <h1 className="profile__title">Привет, Виталий!</h1>
 
           <form className="profile__form">
-            <div className="profile__item">
-              <label for="name" className="profile__lable">
-                Имя
-              </label>
-              <input
-                autoComplete="off"
-                className="profile__input"
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Виталий"
-                disabled={isEditing ? false : true}
-              ></input>
-            </div>
-            <div className="profile__item">
-              <label for="email" className="profile__lable">
-                Почта
-              </label>
-              <input
-                autoComplete="off"
-                className="profile__input"
-                type="email"
-                id="email"
-                name="email"
-                placeholder="pochta@yandex.ru"
-                disabled={isEditing ? false : true}
-              ></input>
+            <div className="profile__items">
+              <div className="profile__item">
+                <label for="name" className="profile__lable">
+                  Имя
+                </label>
+                <input
+                  autoComplete="off"
+                  className="profile__input"
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Виталий"
+                  disabled={isEditing ? false : true}
+                ></input>
+              </div>
+              <div className="profile__item">
+                <label for="email" className="profile__lable">
+                  Почта
+                </label>
+                <input
+                  autoComplete="off"
+                  className="profile__input"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="pochta@yandex.ru"
+                  disabled={isEditing ? false : true}
+                ></input>
+              </div>
             </div>
 
             {isEditing ? (
@@ -50,7 +52,7 @@ function Profile({ isEditing, onEditClick }) {
               </div>
             ) : (
               <div className="profile__account">
-                <button className="profile__edit link" onClick={onEditClick}>
+                <button className="profile__edit" onClick={onEditClick}>
                   Редактировать
                 </button>
                 <Link to="/signin" className="profile__log-out link">
